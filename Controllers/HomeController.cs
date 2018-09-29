87154a0c56df994717bb1c92378d67bbe76c43fd
Lucas <lucaspereira18@gmailcom>
2018-09-29 15:23:35 -0300

    Aula 29/09

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using Fiap01.Data;
+using Fiap01.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fiap01.Controllers
+{
+
+  
+    public class HomeController : Controller
+    {
+
+        PerguntasContext _context { get; set; }
+
+        public HomeController(PerguntasContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Nome = "Lucas";
+            ViewData["NomeDoAluno"] = $"Outro Nome {DateTime.Now}";
+
+            //var pergunta = new Pergunta() { Id = 1, Descricao = "Descricao da pergunta", Autor="Lucas" };
+           
+
+            return View(_context.Perguntas.ToList());
+
+
+            // return View("OutraView");
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public  IActionResult Create(Pergunta pergunta)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.Perguntas.Add(pergunta);
+                _context.SaveChangesAsync();
+
+
+            }
+
+            return View();
+
+        }
+
+        public IActionResult Ajuda()
+        {
+            return View();
+           
+        }
+
+        public IActionResult Sobre()
+        {
+            return View();
+           
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System;
+
+namespace Fiap01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+        }
+    }
+}
+
+
+
